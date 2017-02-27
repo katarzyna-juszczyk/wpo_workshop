c92a3b28cff617530551cc398db9b317357033cf
Mateusz Kwasniewski <kwasniewski.mateusz@gmail.com>
2017-02-27 20:09:03 +0100

    better file structure

--- build-scripts/templateEngine.js
+++ build-scripts/templateEngine.js
@@ -1,8 +1,9 @@
 var fs = require("fs");
 
-var inputDir = "html";
+var inputDir = "src";
 var outputDir = "dist";
 var templateFile = "template.html";
+var extension = ".html";
 var placeholder = "%MAIN_CONTENT%";
 var encoding = "utf8";
 
@@ -10,17 +11,25 @@ function file(dir, fileName) {
     return [dir, fileName].join("/");
 }
 
-fs.readdir("html", function (err, files) {
-    if (err) {
-        throw err;
-    }
-    var templateContent = fs.readFileSync(file(inputDir, templateFile), encoding);
+function supportedFile(name) {
+    return name !== templateFile && name.endsWith(extension);
+}
 
-    files.filter(name => name !== "template.html").forEach(function (name) {
+function panic(err) {
+    if(err) throw err;
+}
+
+function templateContent() {
+    return fs.readFileSync(file(inputDir, templateFile), encoding);
+}
+
+fs.readdir(inputDir, function (err, files) {
+    panic(err);
+    files.filter(supportedFile).forEach(function (name) {
         fs.readFile(file(inputDir, name), encoding, function (err, data) {
             var outputFile = file(outputDir, name);
-            fs.writeFile(outputFile, templateContent.replace(placeholder, data), function (err) {
-                if (err) throw err;
+            fs.writeFile(outputFile, templateContent().replace(placeholder, data), function (err) {
+                panic(err);
                 console.log("Saved ", outputFile);
             });
 
